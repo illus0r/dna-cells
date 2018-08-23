a0a6b7c8932f770729632e500aa2fe3d28cea95d
illus0r <zor667@gmail.com>
2018-08-23 15:27:28 +0300

    something

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 //Сегодня я хочу
 //— ускорить вычисления за счёт перехода к плотной упаковке вселенной (каждая ячейка может содержать только одну клетку.
 //— Продумать структуру, которая поможет организмам соревноваться.
-//— Подправлю цвета
+//
+//v Подправлю цвета
 
 
 function World(height, width) {
-  this.cellSize = 8;
+  this.cellSize = 64;
   this.i = 0;
   this.height = height;
   this.width = width;
   this.parse = function(string){
     //return([320,516,220,320,416,420,120,416,420,620,416,421,621,816,920,020,219]); // cascade
     //return([ 	239,	234,	650, 100,	757,	36,	676,	266, 72,	453,	764,	72,	141, 738,	756,	484,	35,	947, 157,	538,	223,	507,	757, 995,	175,	82,	263,	270, 533,	372,	19,	912,	708, 836,	355,	432,	176,	349, 621,	242,	301,	825,	533, 871,	129,	605,	270,	491, 691,	73,	308,	951,	927, 599,	245,	647,	937,	65, 413,	672,	2,	995,	966, 741,	691,	971,	558,	596, 622,	723,	526,	532,	590, 652,	597,	306,	371,	191, 278,	641,	385,	407,	947, 121,	234,	186,	14,	577, 950,	585,	995,	127,	600, 616,	459,	558,	549,	274, 670,	565,	106,	870,	830, 241,	634,	133,	516,	790 ]); // cascade
-    return([	597,	306,	371,	191, 278,	641,	385,	407,	947, 121,	234,	186,	14,	577, 950,	585,	995,	428,	600, 616,	459,	558,	549,	274, 670,	565,	106,	870,	830, 241,	634,	133,	516,	790 ]); // fireflys
+    return([ 72,	141, 738,	756,	484,	35,	947, 157,	538,	223,	507,	757, 995,	175,	82,	263,	270, 533,	372,	19,	912,	708, 836,	355,	432,	176,	349, 621,	242,	301,	825,	533, 871,	129,	605,	270,	491, 691,	73,	308,	951,	927, 599,	245,	647,	937,	65, 413,	672,	2,	995,	966, 741,	691,	971,	558,	596, 622,	723,	526,	532,	590, 652,	597,	306,	371,	191, 278,	641,	385,	407,	947, 121,	234,	186,	14,	577, 950,	585,	995,	127,	600, 616,	459,	558,	549,	274, 670,	565,	106,	870,	830, 241,	634,	133,	516,	790 ]); // cascade
+    //return([	597,	306,	371,	191, 278,	641,	385,	407,	947, 121,	234,	186,	14,	577, 950,	585,	995,	428,	600, 616,	459,	558,	549,	274, 670,	565,	106,	870,	830, 241,	634,	133,	516,	790 ]); // fireflys
     //return([550,550,550,550,550,550,550,550,550,550,550,550,550]);
   };
   var dna = this.parse(".....x");
@@ -52,7 +54,21 @@ function World(height, width) {
   }
 };
 
-function Animal(dna,x,y){
+function Animal(dna,x,y){//TODO Animal is more wordish now
+  this.boxes = {};
+  this.boxes['1'] = {}
+  this.boxes['2'] = {}
+  this.boxes['3'] = {}
+  this.boxes['4'] = {}
+  this.boxes['1']['2'] = 1;
+  this.boxes['2']['3'] = 2;
+  this.boxes['3']['4'] = 3;
+  this.boxes['4']['4'] = 3;
+  this.boxes['1']['4'] = 3;
+  //console.log(this.boxes.filter(function(d){
+    //return true;
+    //return '2' in obj;
+    //}));
   this.cells = [new Cell(dna,0,x,y)];
   this.develop = function(){
     if(this.cells.length < 200){
@@ -66,25 +82,25 @@ function Animal(dna,x,y){
           var cellsToMove = th.cells.map(function(c){
             if(dy == -1){
               if(c.x == cell.x && c.y < cell.y){
-                console.log("up");
+                //console.log("up");
                 c.y = c.y - 1;
               }
             }
             else if(dx ==  1){
               if(c.x > cell.x && c.y == cell.y){
-                console.log("right");
+                //console.log("right");
                 c.x = c.x + 1;
               }
             }
             else if(dy ==  1){
               if(c.x == cell.x && c.y > cell.y){
-                console.log("down");
+                //console.log("down");
                 c.y = c.y + 1;
               }
             }
             else if(dx == -1){
               if(c.x < cell.x && c.y == cell.y){
-                console.log("left");
+                //console.log("left");
                 c.x = c.x - 1;
               }
             }
@@ -108,8 +124,10 @@ function Cell(dna,cursor,x,y){
   this.y = y;
   this.isDead = false;
   this.colorUpdate = function(){
-    this.color = "hsl("+parseInt(360 * this.cursor / this.dna.length)+", 100%, 50%)";
-    console.log(parseInt(100 * this.cursor / this.dna.length));
+    //this.color = "hsl("+parseInt(360 * this.cursor / this.dna.length)+", 100%, 50%)";
+    //this.color = "hsl("+700+parseInt(60 * this.cursor / this.dna.length)+", 100%, 50%)";
+    this.color = "hsl("+100+parseInt(160 * this.cursor / this.dna.length)+", 100%, 50%)";
+    //console.log(parseInt(100 * this.cursor / this.dna.length));
   };
   this.colorUpdate();
   this.develop = function(){
@@ -122,28 +140,28 @@ function Cell(dna,cursor,x,y){
     var dnaCurrent = this.dna[this.cursor];
     if(dnaCurrent<100){
         // divide up
-        console.log("↑");
+        //console.log("↑");
         dx =  0;
         dy = -1;
         isDivided = true;
     } else
     if(dnaCurrent<200){
         // divide right
-        console.log("→");
+        //console.log("→");
         dx =  1;
         dy =  0;
         isDivided = true;
     } else
     if(dnaCurrent<300){
         // divide down
-        console.log("↓");
+        //console.log("↓");
         dx =  0;
         dy =  1;
         isDivided = true;
     } else
     if(dnaCurrent<400){
         // divide left
-        console.log("←");
+        //console.log("←");
         dx = -1;
         dy =  0;
         isDivided = true;
@@ -152,7 +170,7 @@ function Cell(dna,cursor,x,y){
         // goto
         this.cursor = this.dna[this.cursor] - 1; // to compensate later increase
         this.normalizeCursor();
-        console.log("goto", this.cursor+1);
+        //console.log("goto", this.cursor+1);
     } else
     if(dnaCurrent<600){
       //idle
@@ -180,7 +198,7 @@ function dnaBit(string){
   this.command = stringParts[0];
 }
 
-var world = new World(1000, 1000);
+var world = new World(10000, 10000);
 //var t = d3.timer(function(elapsed) {
 //  console.log(elapsed);
 //}, 2000, 2000);
